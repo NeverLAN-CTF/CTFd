c6dc87821e76e433c1ebd710d21488813144c98b
Dan Daggett <dan@socialgeeks.com>
2016-02-23 07:02:40 +0000

    Update entrypoint to add a delay to wait for the sql server to become available prior to running gunicorn.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,14 @@
 
 sed "s;sqlite:///ctfd.db;mysql+pymysql://${MYSQL_USER}:${MYSQL_PASSWORD}@mariadb/ctfd;g" /opt/CTFd/CTFd/config.py -i
 
+# wait for mysql to start
+while ! nc mariadb 3306 >/dev/null 2>&1 < /dev/null; do
+    if [ $i -ge 50 ]; then
+      echo "$(date) - mariadb:3306 still not reachable, giving up"
+      exit 1
+    fi
+    echo "$(date) - waiting for mariadb:3306..."
+    sleep 1
+done
+
 exec "$@"
